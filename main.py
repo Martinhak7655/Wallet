76715abe4d7673e616701d40ef6fcf11d9d9ad67
Martinhak7655 <martinhakobyan954@gmail.com>
2024-10-06 21:00:34 +0400

    Last Updates

--- main.py
+++ main.py
@@ -4,28 +4,24 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def auth(email, password):
-    if email == "test@gmail.com" or password == "1122":
+random_num = random.randint(1000, 9999)
+def mail():
+    msg = MIMEMultipart()
+    msg['From'] ="thedavitmanukyan@mail.ru"
+    msg['To'] = "martinhakobyan954@gmail.com"
+    msg['Subject'] = "Hastateq dzer mail hascen"
+    msg.attach(MIMEText( f"Mek angamva ogtagorcman kod - {random_num}", 'plain'))
+    server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
+
+    server.login("thedavitmanukyan@mail.ru", "y0xG0CGX5hntimArxVK8")
+
+    server.sendmail("thedavitmanukyan@mail.ru", "martinhakobyan954@gmail.com", msg.as_string())
+
+def auth(email, password, verification):
+    mail()
+    if email == "test@gmail.com" and password == "1122" and verification == random_num:
         return True
     return False
-
-def mail(num):
-   random_num = random.randint(1000, 9999)
-   msg = MIMEMultipart()
-   msg['From'] = 'thedavitmanukyan@mail.ru'
-   msg['To'] = 'martinhakobyan954@gmail.com'
-   msg["Subject"] = 'Hastateq Dzer Mail hascen'
-   
-   msg.attach(MIMEText( f"Mek angamva ogtagorcman kod - {random_num}", 'plain'))
-   server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
-
-   server.login("thedavitmanukyan@mail.ru", "y0xG0CGX5hntimArxVK8")
-
-   server.sendmail("thedavitmanukyan@mail.ru", "martinhakobyan954@gmail.com", msg.as_string())
-   if num == random_num:
-    return True
-   return False
-
 
 def getBallane():
     return userbalance[0]
@@ -34,19 +30,39 @@ def cashin(sum):
     if sum <= 100:
         return False
     userbalance[0] += sum
+    msg = MIMEMultipart()
+    msg['From'] ="thedavitmanukyan@mail.ru"
+    msg['To'] = "martinhakobyan954@gmail.com"
+    msg['Subject'] = "Dzer Hashivy"
+    msg.attach(MIMEText( f"Dzer Hasvin Avelacele - {sum}", 'plain'))
+    server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
+
+    server.login("thedavitmanukyan@mail.ru", "y0xG0CGX5hntimArxVK8")
+
+    server.sendmail("thedavitmanukyan@mail.ru", "martinhakobyan954@gmail.com", msg.as_string())
     return True
 
 def cashout(sum):
     if sum <= 0 and sum > userbalance[0]:
         return False
     userbalance[0] -= sum
+    msg = MIMEMultipart()
+    msg['From'] ="thedavitmanukyan@mail.ru"
+    msg['To'] = "martinhakobyan954@gmail.com"
+    msg['Subject'] = "Dzer Hashivy"
+    msg.attach(MIMEText( f"Dzer Hasvic hanvele - {sum}", 'plain'))
+    server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
+
+    server.login("thedavitmanukyan@mail.ru", "y0xG0CGX5hntimArxVK8")
+
+    server.sendmail("thedavitmanukyan@mail.ru", "martinhakobyan954@gmail.com", msg.as_string())
     return True
 
 while True:
     email = input("Email: ")
     password = input("Password:  ")
-    numb = int(input("Hastateq Email um uxarkvac kody"))
-    if auth(email=email, password=password) and mail(num=numb):
+    numb = int(input("Hastateq emailum grvac kody >>>  "))
+    if auth(email=email, password=password, verification=numb):
         print("-" * 25)
         print("Register Succes")
         print("Check Balance: B")
