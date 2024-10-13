f1d6e5dfaa952416d0662dbe4bc9e9de270fea6a
Martinhak7655 <martinhakobyan954@gmail.com>
2024-10-13 20:44:18 +0400

    Last Updates

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ userbalance = [0]
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+#avelacnel shat ogtaterer 
+
+users = [
+
+]
 
 random_num = random.randint(1000, 9999)
 def mail():
@@ -17,10 +22,29 @@ def mail():
 
     server.sendmail("thedavitmanukyan@mail.ru", "martinhakobyan954@gmail.com", msg.as_string())
 
-def auth(email, password, verification):
-    mail()
-    if email == "test@gmail.com" and password == "1122" and verification == random_num:
+def front(hastatum=None, hastatum2=None):
+    if hastatum == "grancvel":
         return True
+    elif hastatum2 == "mutq gorcel":
+        return True
+    return False
+
+def grancum(email, password, verification):
+    if len(password) >= 8:
+        mail()
+        users_grancum = {
+            "mail": email,
+            "password": password
+        }
+        users.append(users_grancum)
+        if verification == random_num:
+            return True
+    return False
+
+def mutqgorcel(password2):
+    for user in users:
+        if password2 == user["password"]:
+            return True
     return False
 
 def getBallane():
@@ -59,37 +83,74 @@ def cashout(sum):
     return True
 
 while True:
-    email = input("Email: ")
-    password = input("Password:  ")
-    numb = int(input("Hastateq emailum grvac kody >>>  "))
-    if auth(email=email, password=password, verification=numb):
-        print("-" * 25)
-        print("Register Succes")
-        print("Check Balance: B")
-        print("Cashin command: +")
-        print("Cashout command: -")
-        print("Exit: E")
-        print("-" * 25)
-        while True:
-            command = input("Command: ")
-            if command == "B":
-                print(f"Balance: {getBallane()}, dram")
-            elif command == "+":
-                Sum = int(input("chashin: "))
-                if cashin(sum=Sum):
-                    print("Procces Succesed")
-                else:
-                    print("Ann error try again")
-            elif command == "-":
-                Sum = int(input("Cashout: "))
-                if cashout(sum=Sum):
-                    print("Procces Succesed")
-                else:
-                    print("An error try again")
-            elif command == "E":
-                print("Exit")
-                break
+    harc = input("Cankanumeq mutq gorcel te grancvel >>  ")
+    if front(hastatum=harc):
+        email = input("Email: ")
+        password = input("Password:  ")
+        numb = int(input("Hastateq emailum grvac kody >>>  "))
+        if grancum(email=email, password=password, verification=numb):
+            print("-" * 25)
+            print("Register Succes")
+            print("Check Balance: B")
+            print("Cashin command: +")
+            print("Cashout command: -")
+            print("Exit: E")
+            print("-" * 25)
+            while True:
+                command = input("Command: ")
+                if command == "B":
+                    print(f"Balance: {getBallane()}, dram")
+                elif command == "+":
+                    Sum = int(input("chashin: "))
+                    if cashin(sum=Sum):
+                        print("Procces Succesed")
+                    else:
+                        print("Ann error try again")
+                elif command == "-":
+                    Sum = int(input("Cashout: "))
+                    if cashout(sum=Sum):
+                        print("Procces Succesed")
+                    else:
+                        print("An error try again")
+                elif command == "E":
+                    print("Exit")
+                    break
+        else:
+            print("-" * 25)
+            print("Invalid Email or Password")
+            print("-" * 25)
+    elif front(hastatum2=harc):
+        password2 = input("password: ")
+        if mutqgorcel(password2=password2):
+            print("-" * 25)
+            print("Register Succes")
+            print("Check Balance: B")
+            print("Cashin command: +")
+            print("Cashout command: -")
+            print("Exit: E")
+            print("-" * 25)
+            while True:
+                command = input("Command: ")
+                if command == "B":
+                    print(f"Balance: {getBallane()}, dram")
+                elif command == "+":
+                    Sum = int(input("chashin: "))
+                    if cashin(sum=Sum):
+                        print("Procces Succesed")
+                    else:
+                        print("Ann error try again")
+                elif command == "-":
+                    Sum = int(input("Cashout: "))
+                    if cashout(sum=Sum):
+                        print("Procces Succesed")
+                    else:
+                        print("An error try again")
+                elif command == "E":
+                    print("Exit")
+                    break
+        else:
+            print("An error try again")
     else:
         print("-" * 25)
-        print("Invalid Email or Password")
+        print("Aydpisi command chka")
         print("-" * 25)
